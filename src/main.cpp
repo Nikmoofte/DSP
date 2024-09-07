ff02497bf806fafdbbabab003f6e2f41ddff6e29
NotANick <ilya.vailov@gmail.com>
2024-09-07 10:53:20 +0300

    project init

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctime>
+#include <limits>
+#include <print>
+#include <cmath>
+
+
+
+// Main code
+int main(int, char**)
+{
+    std::print("Hello, world!");
+    return 0;
+}
